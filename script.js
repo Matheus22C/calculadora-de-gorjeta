fe2299d9ddcb2f24286898d64e8bb0eb0d50c24d
Matheus22C <162534273+Matheus22C@users.noreply.github.com>
2024-04-04 10:15:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 let conta = 0
-let pessoas = 0
+let pessoa = 0
 let porcentagem = 0
 
 const contaInput = document.querySelector("#conta")
-contaInput.addEventListener("input", receberValorConta )
+contaInput.addEventListener("input", receberValorConta)
 
 function receberValorConta(evento) {
     conta = Number(evento.target.value)
     calcular()
-
-    
 }
 
 const pessoasInput = document.querySelector("#pessoas")
@@ -18,6 +16,7 @@ pessoasInput.addEventListener("input", receberQuantidadePessoas)
 function receberQuantidadePessoas(evento) {
     const paragrafoErro = document.querySelector(".pessoas #erro")
     const divErro = document.querySelector(".pessoas .input-box")
+
     if(evento.target.value === "0") {
         paragrafoErro.style.display = "block"
         divErro.setAttribute("id", "erro-div")
@@ -49,6 +48,7 @@ function receberPorcentagem(evento) {
     } else {
         porcentagem = 0
     }
+
     calcular()
 }
 
@@ -58,7 +58,7 @@ gorjetaInput.addEventListener("input", receberPorcentagem)
 function calcular() {
     if(conta !== 0 && porcentagem !== 0 && pessoas !== 0) {
         const strongGorjetaTotal = document.querySelector(".gorjeta-total > strong")
-        strongGorjetaTotal.innerHTML = R$ ${(conta * porcentagem / pessoas).toFixed(2)}
+        strongGorjetaTotal.innerHTML = R$ ${(conta * porcentagem / pessoas).toFixed(2)}  
 
         const strongTotal = document.querySelector(".total > strong")
         strongTotal.innerHTML = R$ ${((conta + (conta * porcentagem)) / pessoas).toFixed(2)}
@@ -71,20 +71,18 @@ botaoLimpar.addEventListener("click", limpar)
 function limpar() {
     contaInput.value = ""
 
-    botoesGorjeta.forEach(botao =>{
+    botoesGorjeta.forEach(botao => {
         botao.classList.remove("botao-ativo")
     })
-
 
     gorjetaInput.value = ""
 
     pessoasInput.value = ""
 
     document.querySelector(".gorjeta-total > strong").innerHTML = "R$ 0.00"
-
     document.querySelector(".total > strong").innerHTML = "R$ 0.00"
 
     conta = 0
+    pessoa = 0
     porcentagem = 0
-    pessoas = 0
 }
